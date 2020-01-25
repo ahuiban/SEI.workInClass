ca213e293f4cac4c407a1956421ce555921ebd54
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-01-25 14:59:23 -0500

    completed

--- 19. w7d01 - Color Picker and Shape clicker(DOM using events in aloop)/palette_picker_starter/app.js
+++ 19. w7d01 - Color Picker and Shape clicker(DOM using events in aloop)/palette_picker_starter/app.js
@@ -1,28 +1,45 @@
 console.log("app.js loaded for palette picker");
 $(() => {
-    const $colorPalette = $("#color-palette");
-    const $myPalette = $("#my-palette");
-    const $generate = $("#generate");
     const addColor = event => {
-        console.log($(event.currentTarget).css("background-color"));
-        const color = $(event.currentTarget).css("background-color");
-        const $square = $("<div class='square'>");
+        const color = $(event.target).css("background-color");
+
+        const $square = $("<div>");
+        $square.addClass("square");
+        // Or can do both in one line like this:
+        // const $square = $("<div class=square>");
+
+        // Give square the background color of the one user clicked on (event.target):
         $square.css("background-color", color);
+
         $myPalette.append($square);
     };
+
     const makePalette = () => {
         $colorPalette.empty();
         for (let i = 0; i < 150; i++) {
-            const red = Math.floor(Math.random() * 256);
-            const green = Math.floor(Math.random() * 256);
-            const blue = Math.floor(Math.random() * 256);
-            const color = `rgb(${red}, ${green}, ${blue})`;
-            const $square = $("<div class='square'>");
-            $square.css("background-color", color);
+            const $square = $("<div>");
+            $square.addClass("square");
+            // Generate a random hex number between 000000 and FFFFFF:
+            // const randHex = '#' + Math.floor(Math.random() * 16777215).toString(16);
+            // Or easier to understand when done as RGB values:
+            const red = Math.floor(Math.random() * 255);
+            const green = Math.floor(Math.random() * 255);
+            const blue = Math.floor(Math.random() * 255);
+            const randHex = "rgb(" + red + "," + green + "," + blue + ")";
+            // Or with template literal syntax:
+            // const randHex = `rgb(${red},${green},${blue})`;
+            $square.css("background-color", randHex);
+            $square.on("click", addColor);
+
             $colorPalette.append($square);
         }
-        $(".square").on("click", addColor);
     };
+
+
+
+    const $colorPalette = $("#color-palette");
+    const $myPalette = $("#my-palette");
+    const $generate = $("#generate");
+
     $generate.on("click", makePalette);
 });
-
