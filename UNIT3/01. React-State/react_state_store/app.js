e2c498fa88f338155668cf93d1a91ac573c5593d
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-03-18 22:03:21 -0400

    updated

--- UNIT3/01. React-State/react_state_store/app.js
+++ UNIT3/01. React-State/react_state_store/app.js
@@ -1,26 +1,43 @@
-//whole component with multi-line map:
+//Working code using state, passing props (to greeter), using .map, and using an onClick handler
 
 console.log(products);
-
 class Greeter extends React.Component {
-    render () {
-        console.log(this.props);
-        return <h1>Hi there {this.props.name}</h1>;
-    }
+  render() {
+    console.log(this.props);
+    return <h1>Hi there {this.props.name}</h1>;
+  }
 }
 class App extends React.Component {
+  // New method. Much easier:
   state = {
-      products: products,
-      name: "Andrei",
-      developer: "Andrei",
-      breakoutrooms: [1,2,3,4]
+    products: products,
+    name: "Ira",
+    developer: "Dejay",
+    breakoutRooms: [1, 2, 3, 4]
   };
-  render () {
-      return (
-          <div>
-              <Greeter name={this.state.name} location={"Atlanta"} />
-          </div>
-      )
+  handleGreeterClick = () => {
+    console.log("Clicked!");
+    this.setState({ name: "Jordan" });
+  };
+  render() {
+    return (
+      <div>
+        <div onClick={this.handleGreeterClick}>
+          <Greeter name={this.state.name} location={"Los Angeles"} />
+          <a href=""></a>
+        </div>
+        <ul>
+          {this.state.products.map(product => {
+            return (
+              <li>
+                {product.name} {product.price}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
+}
 
 ReactDOM.render(<App />, document.querySelector(".container"));
