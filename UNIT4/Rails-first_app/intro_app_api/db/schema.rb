dc50c280f33f54ed780e078dcccd25ccb4398a23
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-04-22 21:50:45 -0400

    updated

--- UNIT4/Rails-first_app/intro_app_api/db/schema.rb
+++ UNIT4/Rails-first_app/intro_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_010028) do
+ActiveRecord::Schema.define(version: 2020_04_23_004517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_04_22_010028) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
