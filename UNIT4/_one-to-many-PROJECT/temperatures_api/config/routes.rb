8946af1457c9d2a281dc3b4fec480104bbdf21de
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-05-02 11:56:32 -0400

    updated

--- UNIT4/_one-to-many-PROJECT/temperatures_api/config/routes.rb
+++ UNIT4/_one-to-many-PROJECT/temperatures_api/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :temperatures
-  resources :locations
+  resources :temperatures, only: [:index]
+  resources :locations, only: [:index, :show] do 
+    resources :temperatures, only: [:create]
+  
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
